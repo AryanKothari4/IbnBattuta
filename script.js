f285488798dfc90b76fb93923e86ac871d4249b1
AryanKothari4 <aryankonweb@gmail.com>
2025-10-07 18:10:11 -0400

    fixed small bugs

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Get the flip container element from the HTML
+// Why u readin' this man? Go check out the brochure!
 const flipContainer = document.querySelector('.flip-container');
 
-// Add an event listener that waits for a click on the container
 flipContainer.addEventListener('click', () => {
-    // When clicked, toggle the 'flipped' class.
-    // This simple action triggers the CSS animation.
     flipContainer.classList.toggle('flipped');
 });
